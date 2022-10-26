efbf7c40cc24807e326f9625343ca40960aa860b
Lucy Ruiz <lucyruiz846@gmail.com>
2022-10-26 11:33:26 -0700

    Updated search bar using .filter(search)

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import SearchBar from './Components/SearchBar/SearchBar';
 function App() {
 
     const[songs, setSongs] = useState([]);
-    
+    const[search, setSearch] = useState('');
     
     useEffect(() => {
         getAllSongs();
@@ -16,7 +16,20 @@ function App() {
     
     async function getAllSongs(){
         let response = await axios.get('http://127.0.0.1:8000/api/music/');
-        setSongs(response.data);
+        if (search === ''){
+            setSongs(response.data)
+        }
+        else {
+            let results = response.data.filter((song) => {
+                if(song.id.includes(search) || song.title.includes(search) || song.artist.includes(search) || song.album.includes(search) || song.release_date.includes(search) || song.genre.date.includes(search)){
+                    return true;
+                    }
+                else{
+                    return false;
+                    }
+                });
+            setSongs(results)
+            }
     }
 
 
@@ -24,7 +37,7 @@ function App() {
     <div>
       <h3>Music Library</h3>
       <div>
-        <SearchBar/>
+        <SearchBar filteredSongs={setSearch}/>
       </div>
       <div>
         <MusicTable allSongs={songs}/>
--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -4,17 +4,7 @@ const SearchBar = (props) => {
     return(
         <form>
             <label>Filter by:</label>
-            <select>
-                <option>ID</option>
-                <option>Title</option>
-                <option>Artist</option>
-                <option>Album</option>
-                <option>Release Date</option>
-                <option>Genre</option>
-            </select>
-            <br></br>
             <input type='text'></input>
-            <br></br>
             <button type='submit'>Search</button>
         </form>
     );
